40ca473df9e647dd18b77d7e6f66636710cacc51
Pavel Palancica <pavel.palancica@gmail.com>
2025-05-24 16:11:26 +0300

    first commit

---
+++ FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Pavel Palancica  on 24.05.2025.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
